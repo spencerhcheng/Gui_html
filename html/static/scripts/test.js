42cc481c58e4b731caf905af91fc001547b32da5
Kimberly Wong <kjowong@gmail.com>
2017-10-26 03:35:25 -0700

    Fixing axios ajax call

--- html/static/scripts/test.js
+++ html/static/scripts/test.js
@@ -1,18 +1,15 @@
-#!/usr/bin/node
-const request = require('axios');
 
-function myFunction() {
-	x = document.getElementById("myForm").elements[0].value;
+function myFunction () {
+  x = document.getElementById('myForm').elements[0].value;
 
-
-	console.log("Hi there", x)
-	document.getElementById("zip").innerHTML = x;
+  console.log('Hi there', x);
+  document.getElementById('zip').innerHTML = x;
 }
 
-request.get('http://0.0.0.0:5000/search/94118')
-	.then (function (response) {
-		console.log(response.data);
+axios.get('http://0.0.0.0:5000/search/94118')
+	.then(function (response) {
+  console.log(response.data);
 })
-	.catch (function (error) {
-		console.log(error);
+	.catch(function (error) {
+  console.log(error);
 });
